02cd5900d4372cf0d3e762aa83276821cc811a24
taiuanpagini <taiuan.pagini@gmail.com>
2021-04-05 15:36:08 -0300

    alter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const App: React.FC = () => {
-  return (<View><Text>Hello</Text><Text>Teste 2</Text></View>);
+  return (<View><Text>Hello</Text><Text>Teste s2</Text></View>);
 };
 
 export default App;
