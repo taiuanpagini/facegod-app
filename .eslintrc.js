4b3068cc65a872afe90b1cfcaa1492f029935227
taiuanpagini <taiuan.pagini@gmail.com>
2021-04-05 15:58:23 -0300

    alterações

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb', 
-    'prettier',
-    'prettier/react'
+    'prettier'
   ],
   globals: {
     Atomics: 'readonly',
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
-const App: React.FC = () => {
-  return (<View><Text>Hello</Text><Text>Teste s2</Text></View>);
-};
+const App: React.FC = () => (
+  <View>
+    <Text>Hello</Text>
+    <Text>Teste s2</Text>
+  </View>
+)
 
-export default App;
+export default App
