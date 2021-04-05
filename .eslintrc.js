ef9f84e1ca0330961e316cd32f2eabeccca25fc2
taiuanpagini <taiuan.pagini@gmail.com>
2021-04-05 15:33:29 -0300

    Projeto Inicial

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb', 'prettier', 'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', {extensions: ['.js', '.jsx', '.tsx']}],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'no-console': ['error', {allow: ['tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (<View><Text>Hello</Text><Text>Teste 2</Text></View>);
+};
+
+export default App;
